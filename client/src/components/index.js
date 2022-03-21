515f547df6ea8c09e8042e473260836ff2880ab9
Keston Antonio Schmidtgen <84077358+Keston-Antonio-Schmidtgen@users.noreply.github.com>
2022-03-21 09:28:11 +0100

    Update index.js

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -5,3 +5,5 @@ export { default as About } from "./About";
 export { default as Contact } from "./Contact";
 export { default as Login } from "./Login";
 export { default as SignUp } from "./SignUp"; 
+
+/* To simplify importing all the components we have created, we are exporting all components from a single file index.js inside the components directory. */
